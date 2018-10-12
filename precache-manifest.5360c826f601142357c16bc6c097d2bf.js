485b2beeb55c202d9d6ddf679dccfc48970fee98
Derrick Cudd <derrickc@videoideas.com>
2018-10-12 15:48:10 -0400

    Add build files

---
+++ precache-manifest.5360c826f601142357c16bc6c097d2bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5639e1233773075e1175",
+    "url": "/static/js/main.5639e123.chunk.js"
+  },
+  {
+    "revision": "8d7c832ed9e9c4f09ec9",
+    "url": "/static/js/1.8d7c832e.chunk.js"
+  },
+  {
+    "revision": "5639e1233773075e1175",
+    "url": "/static/css/main.c4f3151a.chunk.css"
+  },
+  {
+    "revision": "d9182821d16ce5bcf1e089dd5d29d234",
+    "url": "/index.html"
+  }
+];
