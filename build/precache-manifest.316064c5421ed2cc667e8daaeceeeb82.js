7ae0f3961fa4fa36a671bcf66bcb0bd942896035
Derrick Cudd <derrickc@videoideas.com>
2018-10-15 15:55:24 -0400

    Change homepage to github pages

---
+++ build/precache-manifest.316064c5421ed2cc667e8daaeceeeb82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6557391bde8a3ee7e829",
+    "url": "/static/js/main.6557391b.chunk.js"
+  },
+  {
+    "revision": "8d7c832ed9e9c4f09ec9",
+    "url": "/static/js/1.8d7c832e.chunk.js"
+  },
+  {
+    "revision": "6557391bde8a3ee7e829",
+    "url": "/static/css/main.c4f3151a.chunk.css"
+  },
+  {
+    "revision": "9d6a91c0204e46e39e3a0b5eaba1d29a",
+    "url": "/index.html"
+  }
+];
