79de86a5be21fcc3f7d0504b36dafbd30e8c9e12
Derrick Cudd <derrickc@videoideas.com>
2018-10-15 16:28:37 -0400

    Rebuild for gh-pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/360-image-viewer/precache-manifest.322dba775bc2c217e89d80a29c444546.js"
+  "/360-image-viewer/precache-manifest.ec2686436d14849aff652f60de82640e.js"
 );
 
 workbox.clientsClaim();
