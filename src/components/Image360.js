2894cdb408f0a9bfda0bc04268735c3554384dc9
Derrick Cudd <derrickc@videoideas.com>
2018-10-16 17:21:13 -0400

    Add recursive decoding

--- src/components/Image360.js
+++ src/components/Image360.js
@@ -5,26 +5,42 @@ import React from 'react';
 import { Entity, Scene } from 'aframe-react';
 import styled from 'styled-components';
 
-export default ({ match }) => {
-  const src = decodeURIComponent(match.params.src);
-  return (
-    <Container>
-      <Scene>
-        <Entity
-          primitive="a-camera"
-          id="camera"
-          look-controls={{ reverseMouseDrag: true }}
-        >
-          <Entity primitive="a-cursor" />
-        </Entity>
-        <Entity
-          primitive="a-sky"
-          src={`https://cors-anywhere.herokuapp.com/${src}`}
-        />
-      </Scene>
-    </Container>
-  );
-};
+class Image360 extends React.Component {
+  decodeRecursive(str, times = 0) {
+    const decoded = decodeURIComponent(str);
+
+    if (decoded.includes('%') && times < 20) {
+      return this.decodeRecursive(decoded, times + 1);
+    }
+
+    return decoded;
+  }
+
+  render() {
+    const { match } = this.props;
+
+    // Recursively decode the url
+    const src = decodeRecursive(match.params.src);
+
+    return (
+      <Container>
+        <Scene>
+          <Entity
+            primitive="a-camera"
+            id="camera"
+            look-controls={{ reverseMouseDrag: true }}
+          >
+            <Entity primitive="a-cursor" />
+          </Entity>
+          <Entity
+            primitive="a-sky"
+            src={`https://cors-anywhere.herokuapp.com/${src}`}
+          />
+        </Scene>
+      </Container>
+    );
+  }
+}
 
 const Container = styled.div`
   width: 100%;
